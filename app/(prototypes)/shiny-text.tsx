e52d9d3e4fcd4f5d9778564135a4c87e3d8587d1
Vladimir Guguiev <1524432+vovacodes@users.noreply.github.com>
2024-09-15 22:53:47 +0200

    shiny-text: improve and add gyroscope toggle

--- app/(prototypes)/shiny-text.tsx
+++ app/(prototypes)/shiny-text.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Switch } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Animated, {
   useAnimatedStyle,
@@ -19,7 +19,7 @@ import { RadialGradient } from "react-native-gradients";
 const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
 const gradientColors = [
-  { offset: "0%", color: "rgb(255,255,255)", opacity: "0.5" },
+  { offset: "0%", color: "rgb(255,255,255)", opacity: "0.6" },
   // { offset: "50%", color: "rgb(255,255,255)", opacity: "0.2" },
   { offset: "100%", color: "rgb(255,255,255)", opacity: "0" },
   // { offset: "0%", color: "red", opacity: "1" },
@@ -43,38 +43,53 @@ const { colors, locations } = easeGradient({
 const AnimatedText = Animated.createAnimatedComponent(Text);
 
 export default function ShinyText() {
+  const [animationType, setAnimationType] = useState<"sensor" | "continuous">(
+    "sensor"
+  );
   const progress = useSharedValue(-100);
   const deviceRotation = useAnimatedSensor(SensorType.ROTATION);
 
   const animatedStyle = useAnimatedStyle(() => ({
-    // left: `${progress.value}%`,
-    transform: [
-      { translateX: `${deviceRotation.sensor.value.qy * 300}%` },
-      { translateY: `${deviceRotation.sensor.value.qx * 30}%` },
-    ],
+    transform:
+      animationType === "sensor"
+        ? [
+            { translateX: `${-deviceRotation.sensor.value.qy * 400}%` },
+            { translateY: `${-deviceRotation.sensor.value.qx * 20}%` },
+          ]
+        : [{ translateX: `${progress.value}%` }],
   }));
 
   const parallaxStyle = useAnimatedStyle(() => ({
-    transform: [
-      { translateX: deviceRotation.sensor.value.qy * 10 },
-      { translateY: deviceRotation.sensor.value.qx * 10 },
-    ],
+    transform:
+      animationType === "sensor"
+        ? [
+            { translateX: deviceRotation.sensor.value.qy * 10 },
+            { translateY: deviceRotation.sensor.value.qx * 10 },
+          ]
+        : [],
   }));
 
   // Start the animation
   useEffect(() => {
-    progress.value = withRepeat(
-      withDelay(
-        1000,
-        withTiming(100, { duration: 2000, easing: Easing.inOut(Easing.ease) })
-      ),
-      -1
-    );
-  }, []);
+    if (animationType === "continuous") {
+      progress.value = withRepeat(
+        withDelay(
+          1000,
+          withTiming(100, { duration: 2000, easing: Easing.inOut(Easing.ease) })
+        ),
+        -1
+      );
+    }
+  }, [animationType]);
 
   return (
     <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 32,
+      }}
       edges={["bottom", "left", "right"]}
     >
       <Animated.View style={[{ alignSelf: "center" }, parallaxStyle]}>
@@ -130,7 +145,8 @@ export default function ShinyText() {
                 width: "100%",
                 height: "200%",
                 position: "absolute",
-                top: "-100%",
+                top: "-50%",
+                left: "-100%",
                 // backgroundColor: "green",
               },
               animatedStyle,
@@ -146,6 +162,23 @@ export default function ShinyText() {
           </Animated.View>
         </MaskedView>
       </Animated.View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 40,
+          width: "100%",
+        }}
+      >
+        <Text>Use device rotation sensor: </Text>
+        <Switch
+          value={animationType === "sensor"}
+          onValueChange={(value) =>
+            setAnimationType(value ? "sensor" : "continuous")
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 }
